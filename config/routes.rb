92e88f6a27a89891f66ae807ddb85f89ffa17eae
thais paris <thais.paris.mallez@gmail.com>
2021-11-08 12:55:06 +0100

    08/11 exo 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'ask', to: 'pages#ask'
+  get 'answer', to: 'pages#answer'
+ # verb "url", to: "controller#ask"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
